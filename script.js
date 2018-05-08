f658770b942be609035a9004d27effbd9ffb921c
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2018-05-08 16:59:44 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Sidebar
+
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".side-nav .collapse").on("hide.bs.collapse", function() {
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+    });
+    $('.side-nav .collapse').on("show.bs.collapse", function() {
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");
+    });
+});
+
+
+
+
+
+
+// filter search
+
+function setFilterSearch(filter) {
+   // alert("Not a dick lick");
+
+    var filterSearch = document.getElementById("filterSearch");
+
+    filterSearch.innerHTML = filter;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
