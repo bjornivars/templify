4bfbeecf12ed8ce210c6195ec631449e89c1baa2
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2018-05-16 12:57:40 +0200

    added new JS file: dropdown-menus. Relocated the dropdown-script from all sites into JS file

---
+++ dropdown-menus.js
@@ -0,0 +1,43 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function sidebarDropdown() {
+    document.getElementById("myDropdownSidebar").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn-sidebar')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function contactDropdown() {
+    document.getElementById("myDropdownContact").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
