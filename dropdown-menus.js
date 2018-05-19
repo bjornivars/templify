8772b1b80499ddae346656fcba70a13f87668e85
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2018-05-19 16:10:57 +0200

    sidebar dissapears when click on X-icon

--- dropdown-menus.js
+++ dropdown-menus.js
@@ -19,7 +19,9 @@ window.onclick = function(event) {
     }
 };
 
-
+function removeSide() {
+    document.getElementById("myDropdownSidebar").classList.remove("show");
+}
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
